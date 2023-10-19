76c1e8603590c5fd5099d4be03dbc0f2b9bfcd35
Seungwan Han <qak8883@naver.com>
2023-10-19 19:05:18 +0900

    qweqwe

--- data.tf
+++ data.tf
@@ -1,4 +1,3 @@
-
 data "aws_secretsmanager_secret" "hsw"{
   arn = "arn:aws:secretsmanager:ap-northeast-2:723926525504:secret:hsw-lywuWG"
 }
@@ -10,7 +9,7 @@ data "aws_secretsmanager_secret_version" "by" {
 
 variable "key" {
   default = {
-    id = jsondecode(data.aws_secretsmanager_secret_version.by.secret_string)["AWS_ACCESS_KEY_ID"]
-    pwd = jsondecode(data.aws_secretsmanager_secret_version.by.secret_string)["AWS_SECRET_ACCESS_KEY"]
+    id = data.aws_secretsmanager_secret_version.by.secret_string["AWS_ACCESS_KEY_ID"]
+    pwd = data.aws_secretsmanager_secret_version.by.secret_string["AWS_SECRET_ACCESS_KEY"]
   }
 }
