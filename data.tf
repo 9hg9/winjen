f4c1c59dbebe700587f4d49685d9674141707f42
Seungwan Han <qak8883@naver.com>
2023-10-19 19:11:26 +0900

    asdasd

--- data.tf
+++ data.tf
@@ -1,16 +1,16 @@
-data "aws_secretsmanager_secret" "hsw"{
-  arn = "arn:aws:secretsmanager:ap-northeast-2:723926525504:secret:hsw-lywuWG"
-}
+# data "aws_secretsmanager_secret" "hsw"{
+#   arn = "arn:aws:secretsmanager:ap-northeast-2:723926525504:secret:hsw-lywuWG"
+# }
 
-data "aws_secretsmanager_secret_version" "by" {
-  secret_id     = data.aws_secretsmanager_secret.hsw.id
-  version_stage = "example"
-}
+# data "aws_secretsmanager_secret_version" "by" {
+#   secret_id     = data.aws_secretsmanager_secret.hsw.id
+#   version_stage = "example"
+# }
 
-variable "key" {
-  default = {
-    id = data.aws_secretsmanager_secret_version.by.secret_string["AWS_ACCESS_KEY_ID"]
-    pwd = data.aws_secretsmanager_secret_version.by.secret_string["AWS_SECRET_ACCESS_KEY"]
-  }
-  type = map(string)
-}
+# variable "key" {
+#   default = {
+#     id = data.aws_secretsmanager_secret_version.by.secret_string["AWS_ACCESS_KEY_ID"]
+#     pwd = data.aws_secretsmanager_secret_version.by.secret_string["AWS_SECRET_ACCESS_KEY"]
+#   }
+#   type = map(string)
+# }
